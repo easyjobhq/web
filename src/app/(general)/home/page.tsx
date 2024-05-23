be2478fdf13ac867a1f9a35e70773cce36efe4e5
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-05-23 15:33:52 -0500

    feat: adding the basic structure of the component of professionals

--- src/app/(general)/home/page.tsx
+++ src/app/(general)/home/page.tsx
@@ -3,7 +3,9 @@
 import { Professional } from '@/interfaces/professional';
 import { authService } from '@/services'
 import { AuthService } from '@/services/auth.service'
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
+import kanye from "@/../public/kanye.jpg";
 
 
 
@@ -22,9 +24,36 @@ function HomePage() {
 
   return (
     <div>
-        <h1 className="text-xl font-bold">Profesionales en Cali</h1>
-        
-          {
+      <h1 className="text-xl font-bold w-full mb-5">Resultados de la busqueda</h1>
+      <div className="flex">
+      <section className=" bg-slate-400 w-4/5 p-3">
+          <div className="bg-slate-800 w-full mb-3">
+            <div className="upper-section flex">
+              <Image
+              src={kanye}
+              alt=""  
+              width={200}
+              height={100}
+              />
+              <div>
+                <h2>Kanye West</h2>
+                <p>Plomero de profesion</p>
+              </div>
+            </div>
+            <div className="medium-section">
+              <h3>PENEEE</h3>
+            </div>
+            
+            <div className="upper-section">
+              <h3>asdfasdfa</h3>
+            </div>
+          </div>
+          
+        </section>
+        <section className='flex w-1/5 bg-slate-700'>
+          AYUDA
+        </section>
+            {
             professionals.map((professional) => 
                 <>
                   <li>{professional.id}</li>
@@ -46,6 +75,7 @@ function HomePage() {
 
             )
           }
+      </div>
         
     </div>
   )
