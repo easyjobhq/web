17799f2e8ceb17ff8d2b8d3ba5125f70f923a55d
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-10-02 21:23:17 -0500

    feat: making some aestetic changes in the welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,26 +24,26 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const DummyContent = () => {
   return (
     <>
-      
-          <div
-            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
-          >
-            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
-              <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                Estamos siempre a tu disposición.
-              </span>{" "}
-              Nuestra empresa se basa en conocer al cliente, y en ofrecerle un servicio de calidad y eficaz.
-              Para nosotros es muy importante la satisfacción del cliente, por lo que siempre estamos en constante comunicación con el cliente.
-              Nuestros profesionales están altamente capacitados para resolver cualquier problema que se les presente.
-            </p>
-            <Image
-              src="https://assets.aceternity.com/macbook.png"
-              alt="Macbook mockup from Aceternity UI"
-              height="500"
-              width="500"
-              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
-            />
-          </div>
+
+      <div
+        className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+      >
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          <span className="font-bold text-neutral-700 dark:text-neutral-200">
+            Estamos siempre a tu disposición.
+          </span>{" "}
+          Nuestra empresa se basa en conocer al cliente, y en ofrecerle un servicio de calidad y eficaz.
+          Para nosotros es muy importante la satisfacción del cliente, por lo que siempre estamos en constante comunicación con el cliente.
+          Nuestros profesionales están altamente capacitados para resolver cualquier problema que se les presente.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
     </>
   );
 };
@@ -58,9 +58,9 @@ export default function Home() {
   const [selectedCity, setSelectedCity] = useState('');
   const [selectedSpeciality, setSelectedSpeciality] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    const fetchData = async () =>{
+    const fetchData = async () => {
       const responseCities = await authService.getAllCities();
       setCities(responseCities);
 
@@ -70,21 +70,21 @@ export default function Home() {
 
     fetchData();
 
-  },[])
-  
+  }, [])
+
   const theme = createTheme({
     palette: {
       secondary: {
         main: purple[50],
         dark: '#002884',
       },
-      
+
     },
   });
 
-  
 
-  
+
+
 
   const cards = data.map((card, index) => (
     <Card key={card.src} card={card} index={index} />
@@ -92,76 +92,70 @@ export default function Home() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className='h-screen w-full items-center'>
-        <nav className="h-1/4 flex bg-blue-500 px-80 justify-between">
-            <Link href={"/home"} className="flex mt-10">
-                  <BsTools color="white" size="30" className="mr-5"/>
-                  <h2 className="text-white text-2xl font-bold">Easy Job</h2>
+      <div className="flex flex-col justify-between h-[75vh] bg-blue-500 sm:px-[3%] md:px-[15%]">
+        <nav className="flex justify-between items-center pt-5">
+          <Link href={"/home"} className="flex">
+            <BsTools color="white" size="30" className="mr-5" />
+            <h2 className="text-white text-2xl font-bold">Easy Job</h2>
+          </Link>
+          <div id="left-elements" className="flex justify-between items-center space-x-4">
+            <Link href={"/login"}>
+              <p className="text-white text-l">Inicia Sesión</p>
             </Link>
-            <div id="left-elements" className="flex items-start justify-between mt-10 space-x-4">
-              <Link href={"/login"}>
-                <h2 className="text-white text-l">Inicia Sesión</h2>
-              </Link>
-              <Button variant="contained" color="secondary" href="/register" className='hover:bg-blue-500'>
-                ¿Eres un profesional?
-              </Button>
-            </div>
+            <Button
+              variant="contained"
+              color="secondary"
+              href="/register"
+              className='hover:bg-yellow-300'
+              style={{ textTransform: 'none' }}
+            >
+              ¿Eres un profesional?
+            </Button>
+          </div>
         </nav>
-        <nav className="h-1/2 flex-col bg-blue-500 px-80 items-center space-y-4">
-          
-          <h1 className='text-white text-3xl font-bold'>Encuentra un profesional en solo unos clicks</h1>
+        <nav className="flex-col items-end mb-32 space-y-4">
+          <h1 className='text-white text-4xl font-bold'>Encuentra un profesional en solo unos clicks</h1>
           <h4 className='text-gray-300 text-l'>Mas de * profesionales estan aqui para ayudarte</h4>
-         <div id='selects' className='flex space-x-4 bg-blue-700 p-3 rounded-lg'>
-          <select className="bg-white rounded-lg p-4 w-1/3" 
-          value={selectedSpeciality} 
-          onChange={(event: ChangeEvent<HTMLSelectElement>) => {
-              setSelectedSpeciality(event.target.value);
-              
-          }}
-          
-          >
-            <option value='No_selected'>Escoge un filtro</option>
-            {
-              specialities.map((speciality) => (
+          <div id='selects' className='w-4/5 inline-flex space-x-4 bg-blue-700 p-3 rounded-lg'>
+            <select className="bg-white rounded-lg p-4 flex-grow"
+              value={selectedSpeciality}
+              onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                setSelectedSpeciality(event.target.value);
+              }}
+            >
+              <option value='No_selected'>Escoge un filtro</option>
+              {specialities.map((speciality) => (
                 <option key={speciality.id} value={speciality.speciality_name}>{speciality.speciality_name}</option>
-              ))
-            }
-            
-            
-          </select>
-          <select className="bg-white rounded-lg p-3 w-1/4"
-          value={selectedCity} 
-          onChange={(event: ChangeEvent<HTMLSelectElement>) => {
-              setSelectedCity(event.target.value);
-              
-          }}
-          >
-            <option value="No_selected">Escoje un filtro</option>
-            {
-              cities.map((city) => (
+              ))}
+            </select>
+
+            <select className="bg-white rounded-lg p-3 flex-grow"
+              value={selectedCity}
+              onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                setSelectedCity(event.target.value);
+              }}
+            >
+              <option value="No_selected">Escoje un filtro</option>
+              {cities.map((city) => (
                 <option key={city.id} value={city.city_name}>{city.city_name}</option>
-              ))
-            }
-          </select>
-          <Button variant="contained" href="/register" className='flex space-x-2 bg-yellow-300 hover:bg-[#2196f3]'>
-                <FaSearch className='text-black'/>
-                <p className='text-black'>Buscar</p>
-          </Button>
-        </div> 
+              ))}
+            </select>
+
+            <Button variant="contained" href="/home" className='flex space-x-2 bg-yellow-300 hover:bg-yellow-400' style={{ textTransform: 'none', padding: '0 2rem' }}>
+              <FaSearch className='text-black' />
+              <p className='text-black'>Buscar</p>
+            </Button>
+          </div>
         </nav>
-        <div className='h-1/2 items-center flex flex-col'>
-            <TextGenerateEffect words={words} />
-            <Carousel items={cards} />
-            <div className='w-full'> {/* Añadido w-full */}
-              <Footer />
-            </div>
-        </div>
-        
-          
-        
-        
       </div>
-    </ThemeProvider> 
+      <div className='items-center flex flex-col'>
+        <TextGenerateEffect words={words} />
+        <Carousel items={cards} />
+      </div>
+      <div className='w-full'> {/* Añadido w-full */}
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
@@ -184,7 +178,7 @@ const data = [
     src: "https://images.pexels.com/photos/2310904/pexels-photo-2310904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     content: <DummyContent />,
   },
- 
+
   {
     category: "Eficiencia",
     title: "Eficiencia en cada uno de nuestros trabajos",
--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -52,7 +52,7 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
+      <div className="mt-50 md:mt-16">
         <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
           {renderWords()}
         </div>
