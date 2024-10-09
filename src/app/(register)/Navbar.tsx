282acf20c928e800f8331cac80cf297f26083976
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-10-09 10:11:40 -0500

    feat: making responsive the register and login

--- src/app/(register)/Navbar.tsx
+++ src/app/(register)/Navbar.tsx
@@ -6,8 +6,8 @@ import { BsTools } from 'react-icons/bs';
 
 function NavbarRegister() {
   return (
-    <nav className="flex bg-blue-500 px-80 py-4 items-center justify-between">
-      <Link href={"/home"} className="flex">
+    <nav className="flex bg-blue-500 px-5 md:px-[10%] lg:px-[15%] py-4 items-center justify-between">
+      <Link href={"/"} className="flex">
         <BsTools color="white" size="30" className="mr-5" />
         <h2 className="text-white text-2xl font-bold">Easy Job</h2>
       </Link>
